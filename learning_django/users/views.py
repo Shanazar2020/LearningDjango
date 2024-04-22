3a829c59015fac7d92316a93a000c8536dffd997
Shanazar <snizamaddinow@gmail.com>
2024-04-22 16:50:02 +0300

    feature: add user login/logout pages.

--- learning_django/users/views.py
+++ learning_django/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import logout
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -8,9 +10,18 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, f'Your account has been created! You are now able to log in.')
 
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required()
+def profile(request):
+    return render(request, 'users/profile.html')
+
+@login_required()
+def logout_view(request):
+    logout(request)
+    return render(request, 'users/logout.html')
